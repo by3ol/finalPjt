534274988f0cd6b0388e2ed8485234bb2f41507e
softclean7 <rudgh5770@naver.com>
2021-03-12 09:11:19 +0900

    프론트엔드 수정 및 백엔드 기초 taste mapper 변경

--- final/src/main/java/com/kh/drommetur/taste/controller/TasteController.java
+++ final/src/main/java/com/kh/drommetur/taste/controller/TasteController.java
@@ -1,5 +1,14 @@
 package com.kh.drommetur.taste.controller;
 
-public class TasteController {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.kh.drommetur.taste.model.service.TasteService;
+
+public class TasteController {
+	
+	@Autowired
+	private TasteService tasteservice;
+	
+	
+	
 }
--- final/src/main/java/com/kh/drommetur/taste/model/vo/Taste.java
+++ final/src/main/java/com/kh/drommetur/taste/model/vo/Taste.java
@@ -8,18 +8,20 @@ import lombok.Data;
 public class Taste {
 
 	private int placeNo;
-	private String placeAddr;
-	private String placeName;
-	private String placePhone;
-	private String placeGroup;
-	private Double placeLon;
-	private Double placeLat;
-	private String placeCategory;
-	private Date placeDate;
-	private String placeStatus;
-	
-	private int regionNo; //지역번호 
-
+	   private String placeAddr;
+	   private String placeName;
+	   private String placePhone;
+	   private String placeGroup;
+	   private Double placeLon;
+	   private Double placeLat;
+	   private String placeCategory;
+	   private Date placeDate;
+	   private String placeStatus;
+	   private String placeUrl;
+	   private int placeCount;
+	   private double placeScore;
+	   
+	   private int regionNo; //지역번호 
 	
 
 	
