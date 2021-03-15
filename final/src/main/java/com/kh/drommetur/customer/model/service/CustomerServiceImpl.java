161b308654e04f2181d6421530c46ec8db911b23
junghunchoi <azzudi@naver.com>
2021-03-15 09:14:05 +0900

    화면단은 데스크톱으로 해서 16일날 커밋하고 노트북에 작업한 것만 올리갰습니다 // 3월 15일 정훈

--- final/src/main/java/com/kh/drommetur/customer/model/service/CustomerServiceImpl.java
+++ final/src/main/java/com/kh/drommetur/customer/model/service/CustomerServiceImpl.java
@@ -1,5 +1,28 @@
 package com.kh.drommetur.customer.model.service;
 
-public class CustomerServiceImpl {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.kh.drommetur.customer.model.dao.CustomerDao;
+import com.kh.drommetur.customer.model.vo.Question;
+
+@Service("customerService")
+public class CustomerServiceImpl implements CustomerService{
+
+	@Autowired
+	private SqlSessionTemplate slqSession;
+
+	@Autowired
+	private CustomerDao customerDao;
+	
+	
+	@Override
+	public int insertQuestion(Question q) {
+		// TODO Auto-generated method stub
+		return customerDao.insertQuestion(slqSession, q);
+	}
+	
+	
+	
 }
