bc324f378b59363087ba7b3fc76c1c918038a903
Heewon <astronomy307@naver.com>
2021-03-16 09:12:59 +0900

    2021.03.16 CSS 전체 수정

--- final/src/main/java/com/kh/drommetur/member/controller/MemberController.java
+++ final/src/main/java/com/kh/drommetur/member/controller/MemberController.java
@@ -1,6 +1,17 @@
 package com.kh.drommetur.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.drommetur.member.model.service.MemberService;
+
+@Controller
 public class MemberController {
 	
 	
+	@RequestMapping("login.mem")
+	public String loginForm() {
+		return "member/memberLoginForm";
+	}
 }
