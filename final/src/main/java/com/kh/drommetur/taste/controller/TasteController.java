6fa4c93ac523696a45cd9a6fadac3dedc11f24a8
softclean7 <rudgh5770@naver.com>
2021-03-17 09:07:11 +0900

    오류 수정완료

--- final/src/main/java/com/kh/drommetur/taste/controller/TasteController.java
+++ final/src/main/java/com/kh/drommetur/taste/controller/TasteController.java
@@ -46,7 +46,7 @@ public class TasteController {
 		int listCount = tasteService.selectListCount();
 		System.out.println(listCount);
 		
-		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 10, 9);
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 30, 12);
 		
 		ArrayList<Taste> list = tasteService.selectList(pi);
 		
