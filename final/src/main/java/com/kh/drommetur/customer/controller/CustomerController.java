fbd7d132fde7dbdb2e610b21e29b07cde59d2c4e
junghunchoi <azzudi@naver.com>
2021-03-22 09:23:03 +0900

    3월 22일 문의글 관리만 해결하면 끝 / 공지(수정, 삭제)남았음 화면단은 좀 더꾸밀 필요가 있음 / 리뷰는 화면은 거의 다 구현했고 이제 기능완성하면 됨 by 정훈

--- final/src/main/java/com/kh/drommetur/customer/controller/CustomerController.java
+++ final/src/main/java/com/kh/drommetur/customer/controller/CustomerController.java
@@ -1,6 +1,6 @@
 package com.kh.drommetur.customer.controller;
 
-import java.io.File;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,10 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.drommetur.customer.model.service.CustomerService;
+import com.kh.drommetur.customer.model.vo.Notice;
+
 import com.kh.drommetur.customer.model.vo.Question;
+import com.kh.drommetur.taste.Pagination;
+import com.kh.drommetur.taste.model.vo.PageInfo;
+
 
 @Controller
 public class CustomerController {
@@ -34,9 +39,9 @@ public class CustomerController {
 		return "customer/customersaylist";
 	}
 
-	@RequestMapping("customerdetail.cu")
+	@RequestMapping("customernoticedetail.cu")
 	public String customerdetail() {
-		return "customer/customerdetail1";
+		return "customer/customernoticedetail";
 	}
 
 	@RequestMapping("insert.cu")
@@ -61,4 +66,20 @@ public class CustomerController {
 	 * mv.addObject("q", q).setViewName("board/customersaydetail"); } else { throw
 	 * new Exception("문의 상세조회에 실패 하였습니다."); } return mv; }
 	 */
+	
+	@RequestMapping("noticelist.bo")
+	public String selectList(@RequestParam(value = "currentPage", required = false, defaultValue = "1") int currentPage,
+			Model model) {
+
+		
+		int listCount = customerservice.selectListCount();
+
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 10, 5);
+
+		ArrayList<Notice> list = customerservice.selectList(pi);
+
+		model.addAttribute("list", list);
+		model.addAttribute("pi", pi);
+		return "board/customernoticelist";
+	}
 }
--- final/src/main/java/com/kh/drommetur/customer/model/dao/CustomerDao.java
+++ final/src/main/java/com/kh/drommetur/customer/model/dao/CustomerDao.java
@@ -1,9 +1,14 @@
 package com.kh.drommetur.customer.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.drommetur.customer.model.vo.Notice;
 import com.kh.drommetur.customer.model.vo.Question;
+import com.kh.drommetur.taste.model.vo.PageInfo;
 
 @Repository("boardDao")
 public class CustomerDao {
@@ -16,6 +21,21 @@ public class CustomerDao {
 	public int selectQuestion(SqlSessionTemplate sqlSession, int questionNo) {
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne("customerMapper.selectBoard", questionNo);
+	}
+
+	public int selectListCount(SqlSessionTemplate sqlSession) {
+		// TODO Auto-generated method stub
+		
+		return sqlSession.selectOne("customerMapper.selectListCount");
+
+	}
+
+	public ArrayList<Notice> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		// TODO Auto-generated method stub
+		int offset = (pi.getCurrentPage() -1)* pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		
+		return (ArrayList)sqlSession.selectList("customerMapper.selectList", null, rowBounds);
 	}
 
 }
